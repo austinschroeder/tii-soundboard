ddbe40359b326943bc20ac6592d0fb44b6dc955f
austinschroeder <austinsoundy@gmail.com>
2021-07-26 14:44:50 -0700

    Added HTML elements.  Working on JS

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('it works')
+// const buttonOne = document.getElementById('1')
+// console.log(buttonOne.id)
+// const soundOne = new Audio('./audio.one.mp3')
+
+// buttonOne.addEventListener('click', () => soundOne.play())
+
+function playOne() {
+    const audio = document.getElementById("1");
+    audio.play();
+}
+
+function playTwo() {
+    const audio = document.getElementById("2");
+    audio.play();
+}
+
+function playThree() {
+    const audio = document.getElementById("3");
+    audio.play();
+}
+  
+function playFour() {
+    const audio = document.getElementById("4");
+    audio.play();
+}  
+
+function playFive() {
+    const audio = document.getElementById("5");
+    audio.play();
+}
+
+function playSix() {
+    const audio = document.getElementById("6");
+    audio.play();
+} 

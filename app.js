5f9ceb02edc9d9b821e5c7b85d388286a2b8d444
austinschroeder <austinsoundy@gmail.com>
2021-07-27 09:50:36 -0700

    Syncing up for pull

--- app.js
+++ app.js
@@ -4,44 +4,59 @@
 
 // buttonOne.addEventListener('click', () => soundOne.play())
 
-function playOne() {
-    const audio = document.getElementById("1");
-    audio.play();
-}
-
-function playTwo() {
-    const audio = document.getElementById("2");
-    audio.play();
-}
-
-function playThree() {
-    const audio = document.getElementById("3");
-    audio.play();
-}
-  
-function playFour() {
-    const audio = document.getElementById("4");
-    audio.play();
-}  
-
-function playFive() {
-    const audio = document.getElementById("5");
-    audio.play();
-}
-
-function playSix() {
-    const audio = document.getElementById("6");
-    audio.play();
-} 
-
-// const fileList = [
-//     "./audio/one.mp3",
-//     "./audio/two.mp3",
-//     "./audio/three.mp3",
-//     "./audio/four.mp3",
-//     "./audio/five.mp3",
-// ]
-
-// const buildButtons = () => {
-
+// function playOne() {
+//     const audio = document.getElementById("1");
+//     audio.play();
 // }
+
+// function playTwo() {
+//     const audio = document.getElementById("2");
+//     audio.play();
+// }
+
+// function playThree() {
+//     const audio = document.getElementById("3");
+//     audio.play();
+// }
+  
+// function playFour() {
+//     const audio = document.getElementById("4");
+//     audio.play();
+// }  
+
+// function playFive() {
+//     const audio = document.getElementById("5");
+//     audio.play();
+// }
+
+// function playSix() {
+//     const audio = document.getElementById("6");
+//     audio.play();
+// } 
+
+const fileList = {
+    one: "./audio/one.mp3",
+    two: "./audio/two.mp3",
+    three: "./audio/three.mp3",
+    four: "./audio/four.mp3",
+    five: "./audio/five.mp3",
+    six: "./audio/six.mp3",
+}
+
+const playAudio = (fileName) => {
+    new Audio(fileName).play()
+    
+}
+
+const buildButtons = () => {
+    Object.keys(fileList).forEach((soundName, index) => {
+        const fileName = fileList[soundName];
+        const button = document.createElement('button');
+        button.classList.add('btn');
+        button.onclick = () => playAudio(fileName);
+        button.innerText = soundName;
+        document.getElementById('btn-container').appendChild(button)
+    }) 
+}
+
+window.onload = buildButtons;
